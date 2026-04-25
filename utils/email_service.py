6a11cb0cad358b56965b8edf82e73d545a2f6c82
Renato-SI <renatorrbf@gmail.com>
2026-04-25 08:06:02 -0300

    add: email_service.py

---
+++ utils/email_service.py
@@ -0,0 +1,7 @@
+import smtplib
+import random
+import time
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from colorama import Fore
+from utils.utilities import colorir
